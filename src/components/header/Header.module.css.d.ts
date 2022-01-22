e409eb076e21d7520bba2cd5895db4d632f16537
Sam Chan <samchan148@gmail.com>
2022-01-22 16:05:16 +0800

    chore: add css ts types

--- src/components/header/Header.module.css.d.ts
+++
@@ -1,4 +0,0 @@
-export const root: string;
-export const titleWrapper: string;
-export const title: string;
-export const titleLink: string;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 import { Link } from "gatsby";
 
-import * as styles from "./Header.module.css";
+import styles from "./Header.module.css";
 
 import type { PropsWithoutRef } from "react";
 
