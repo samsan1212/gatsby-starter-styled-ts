4ca95766941cc9f4d12df3e8ce611cbf09011cee
Sam Chan <samchan148@gmail.com>
2022-03-16 18:22:36 +0800

    feat: add jest

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,29 @@
+const fs = require("fs");
+const swcConfig = JSON.parse(fs.readFileSync("./.swcrc", { encoding: "utf8" }));
+
 module.exports = {
   transform: {
-    "^.+\\.[jt]sx?$": "<rootDir>/jest-preprocess.js",
+    "^.+\\.[jt]sx?$": ["@swc/jest", swcConfig],
   },
   rootDir: ".",
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
+    "^gatsby-page-utils/(.*)$": `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
+    "^gatsby-core-utils/(.*)$": `gatsby-core-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
+    "^gatsby-plugin-utils/(.*)$": [
+      `gatsby-plugin-utils/dist/$1`,
+      `gatsby-plugin-utils/$1`,
+    ], // Workaround for https://github.com/facebook/jest/issues/9771
     "@/(.*)$": "<rootDir>/src/$1",
   },
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  testEnvironment: "jsdom",
   globals: {
     __PATH_PREFIX__: ``,
   },
   testURL: `http://localhost`,
   setupFiles: [`<rootDir>/loadershim.js`],
+  setupFilesAfterEnv: [`<rootDir>/jest.setup.js`],
 };
---
+++ src/components/header/header.test.tsx
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+
+import { Header } from "./header";
+
+describe(`Header`, () => {
+  it(`renders siteTitle`, () => {
+    const siteTitle = `Hello World`;
+    const { getByText } = render(<Header siteTitle={siteTitle} />);
+
+    const title = getByText(siteTitle);
+
+    expect(title).toBeInTheDocument();
+  });
+});
