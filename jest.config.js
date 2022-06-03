9e36fc2d400dabd1bfe400d5b4b620fe40c55dab
Sam Chan <samchan148@gmail.com>
2022-06-03 15:21:24 +0800

    fix: fix jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-const fs = require("fs");
-
 module.exports = {
   transform: {
     "^.+\\.[jt]sx?$": [
@@ -21,7 +19,7 @@ module.exports = {
               runtime: "automatic",
             },
           },
-          target: "es2017",
+          target: "es2020",
         },
       },
     ],
@@ -30,24 +28,25 @@ module.exports = {
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
-    "^gatsby-page-utils/(.*)$": `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
-    "^gatsby-core-utils/(.*)$": `gatsby-core-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
+    "^gatsby-page-utils/(.*)$": `gatsby-page-utils/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
+    "^gatsby-core-utils/(.*)$": `gatsby-core-utils/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
     "^gatsby-plugin-utils/(.*)$": [
       `gatsby-plugin-utils/dist/$1`,
       `gatsby-plugin-utils/$1`,
     ], // Workaround for https://github.com/facebook/jest/issues/9771
-    "~services/(.*)$": "<rootDir>/src/services/$1",
-    "~components/(.*)$": "<rootDir>/src/components/$1",
+    "~modules/(.*)$": "<rootDir>/src/modules/$1",
     "~assets/(.*)$": "<rootDir>/src/assets/$1",
     "~utils/(.*)$": "<rootDir>/src/utils/$1",
   },
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script)/)`],
   testEnvironment: "jsdom",
   globals: {
     __PATH_PREFIX__: ``,
   },
-  testURL: `http://localhost`,
+  testEnvironmentOptions: {
+    url: `http://localhost`,
+  },
   setupFiles: [`<rootDir>/loadershim.js`],
   setupFilesAfterEnv: [`<rootDir>/jest.setup.js`],
 };
