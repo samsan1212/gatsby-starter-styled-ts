a16eceac0c6007238082e4e36ea59474655c1496
Sam Chan <samchan148@gmail.com>
2022-09-14 17:58:48 +0800

    fix: add service worker reload handling

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -5,3 +5,13 @@ import type { GatsbyBrowser } from "gatsby";
 export const wrapRootElement: GatsbyBrowser["wrapRootElement"] = (props) => {
   return <WrappedRootElement {...props} />;
 };
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
